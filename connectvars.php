b938a90a82f717a1efa39cdd63e0d8373dbfe902
JRS24 <38285782+JRS24@users.noreply.github.com>
2018-04-11 11:20:20 -0500

    Delete connectvars.php

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Define database connection constants
-	DEFINE ('DB_USER', 'fakeUserName');
-	DEFINE ('DB_PASSWORD', 'fakePassword');
-	DEFINE ('DB_HOST', 'localhost');
-	DEFINE ('DB_NAME', 'fakeName');
-?>
