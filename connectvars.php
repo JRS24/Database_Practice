b9f626695503c5a5b72fbcd8ce16e337edb0c10f
JRS24 <38285782+JRS24@users.noreply.github.com>
2018-04-11 08:09:27 -0500

    Create connectvars.php

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+	// Define database connection constants
+	DEFINE ('DB_USER', 'fakeUserName');
+	DEFINE ('DB_PASSWORD', 'fakePassword');
+	DEFINE ('DB_HOST', 'localhost');
+	DEFINE ('DB_NAME', 'fakeName');
+?>
